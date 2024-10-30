541fede6a915dcc1fa830f922fed67c7a438887a
calvindany <119773802+calvindany@users.noreply.github.com>
2024-10-30 22:19:20 +0700

    added: [FE] login page

--- frontend-issue-nest/src/pages/login/Login.jsx
+++ frontend-issue-nest/src/pages/login/Login.jsx
@@ -1,13 +1,57 @@
-import { Container, Typography } from '@mui/material';
+import * as React from "react";
+import axios from "axios";
+import { Container, Typography, TextField } from "@mui/material";
 
-export default function Login(){
-    return (
-        <>
-            <Container className="bg-blue">
-                <div className="min-w-[300px] max-w-[600px]">
-                    <Typography type="h1">Login Page</Typography>
-                </div>
-            </Container>
-        </>
-    )
+import { PasswordField, DefaultButton } from "../../components";
+
+export default function Login() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleSubmit = () => {
+    const requestBody = {
+      email: email,
+      password: password,
+    };
+    axios
+      .post(`${import.meta.env.VITE_API_BASE_URL}/auth`, requestBody)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <>
+      <Container className="h-[100vh] flex flex-col items-center justify-center">
+        <div className="flex flex-col gap-12 min-w-[400px] max-w-[600px] border border-1 shadow-md rounded-md p-12">
+          <Typography variant="p" className="font-bold text-primary-i text-2xl">
+            Login Page
+          </Typography>
+
+          <div className="flex flex-col gap-4">
+            <TextField
+              required
+              id="outlined-required"
+              label="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <PasswordField value={password} onChange={setPassword} />
+
+            <div className="flex justify-end">
+              <DefaultButton
+                variant="contained"
+                type="primary"
+                onclick={handleSubmit}
+              >
+                Login
+              </DefaultButton>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 }
