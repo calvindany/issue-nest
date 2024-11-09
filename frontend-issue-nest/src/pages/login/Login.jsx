246909aa35c20e277952b03c245578c9c9d3b1b3
Calvin Danyalson <119773802+calvindany@users.noreply.github.com>
2024-11-09 15:21:17 +0700

    fixed: [FE] login page layout and experience
    Added:
    - Toast loading when submiting form
    - Mobile Responsive
    - Enter key handler when submit form

--- frontend-issue-nest/src/pages/login/Login.jsx
+++ frontend-issue-nest/src/pages/login/Login.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import axios from "axios";
+import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { Container, Typography, TextField } from "@mui/material";
 
@@ -11,7 +12,24 @@ export default function Login() {
   const [password, setPassword] = React.useState("");
   const navigate = useNavigate();
 
+  React.useEffect(() => {
+    const handleKeyDown = (event) => {
+      if(event.key == "Enter") {
+        if(email.trim() && password.trim()){
+          handleSubmit();
+        }
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown)
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown)
+    }
+  }, [email, password])
+
   const handleSubmit = () => {
+    const loadingToast = toast.loading("Please Wait...")
     const requestBody = {
       email: email,
       password: password,
@@ -21,21 +39,27 @@ export default function Login() {
       .then((res) => {
         const data = res.data.result;
         userLocalStorage.save(data);
-        if (data.role == "Admin") {
-          navigate("/admin/tickets");
-        } else if (data.role == "Client") {
-          navigate("/client/tickets");
-        }
+
+        setTimeout(() => {
+          if (data.role == "Admin") {
+            navigate("/admin/tickets");
+          } else if (data.role == "Client") {
+            navigate("/client/tickets");
+          }
+        }, 2000)
+
+        toast.success("Login Success", { id: loadingToast });
       })
       .catch((err) => {
         console.log(err);
+        toast.error(`${err.response.data.message}`, { id: loadingToast });
       });
   };
   return (
     <>
       <Container className="h-[100vh] flex flex-col items-center justify-center">
-        <div className="flex flex-col gap-12 min-w-[400px] max-w-[600px] border border-1 shadow-md rounded-md p-12">
-          <Typography variant="p" className="font-bold text-primary-i text-2xl">
+        <div className="flex flex-col gap-12 w-[300px] sm:w-[300px] md:w-[500px] border border-1 shadow-xl rounded-2xl px-4 md:px-12 py-14 md:py-20">
+          <Typography variant="p" className="font-500 text-primary-i text-2xl">
             Login Page
           </Typography>
 
@@ -49,7 +73,7 @@ export default function Login() {
             />
             <PasswordField value={password} onChange={setPassword} />
 
-            <div className="flex justify-end">
+            <div className="flex justify-end mt-4">
               <DefaultButton
                 variant="contained"
                 type="primary"
