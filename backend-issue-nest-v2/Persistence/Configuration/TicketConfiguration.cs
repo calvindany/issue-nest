8f76ca0fbd60cce9dab1d738cf6e5d0403a408a1
Calvin Danyalson <119773802+calvindany@users.noreply.github.com>
2024-11-21 10:10:05 +0700

    added: [BE] re-init be project with ignore package files

---
+++ backend-issue-nest-v2/Persistence/Configuration/TicketConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using backend_issue_nest_v2.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace backend_issue_nest_v2.Persistence.Configuration
+{
+    public class TicketConfiguration: IEntityTypeConfiguration<Ticket>
+    {
+        public void Configure(EntityTypeBuilder<Ticket> builder)
+        {
+            builder.ToTable($"{nameof(Ticket)}s");
+            builder.HasOne(x => x.User)
+                .WithMany()
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ backend-issue-nest-v2/Persistence/DbContext.cs
@@ -0,0 +1,41 @@
+﻿using backend_issue_nest_v2.Models;
+using backend_issue_nest_v2.Persistence.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_issue_nest_v2.Persistence
+{
+    public class CustDbContext(DbContextOptions<CustDbContext> options) : DbContext(options)
+    {
+        public  DbSet<Ticket> Tickets { get; set; }
+        public DbSet<User> User { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TicketConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            DateTime saveDate = DateTime.Now.ToLocalTime();
+            var entries = ChangeTracker.Entries().Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+            foreach (var entry in entries)
+            {
+                // updated entry
+                if (entry.Metadata.FindProperty("ModifiedAt") != null)
+                    entry.Property("ModifiedAt").CurrentValue = saveDate;
+
+                if (entry.State == EntityState.Added)
+                {
+                    // created entry
+                    if (entry.Metadata.FindProperty("CreatedAt") != null)
+                        entry.Property("CreatedAt").CurrentValue = saveDate;
+                }
+            }
+
+            return (await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false));
+        }
+    }
+}
