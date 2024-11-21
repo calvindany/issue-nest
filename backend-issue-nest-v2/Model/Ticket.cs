b87d1099a37a72faccc93e723739d46a8cd97327
Calvin Danyalson <119773802+calvindany@users.noreply.github.com>
2024-11-21 09:57:33 +0700

    added: [BE] new api project repo for ef implementation

---
+++ backend-issue-nest-v2/Model/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_issue_nest.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Status { get; set; }
+        public int UserId {  get; set; }
+        [ForeignKey("UserId")]
+        public virtual User? User { get; set; }
+        public string AdminResponse { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ backend-issue-nest-v2/Model/User.cs
@@ -0,0 +1,14 @@
+﻿namespace backend_issue_nest.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string RoleName { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
